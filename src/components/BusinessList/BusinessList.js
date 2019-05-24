cda775afea3ca5d80c5b191bfd4bf36247b0ff50
MFAC <ninafroes@gmail.com>
2019-05-24 13:30:01 +0200

    style: Fix bugs

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,22 +1,15 @@
 import React from "react";
+import "./BusinessList.css";
 import Business from "../Business/Business";
-import styled from "styled-components";
-
-const BusinessListDiv = styled.div`
-  display: flex;
-  justify-content: space - around;
-  flex-wrap: wrap;
-  margin: 4.4rem 10 %;
-`;
 
 class BusinessList extends React.Component {
   render() {
     return (
-      <BusinessListDiv className="BusinessList">
+      <div className="BusinessList">
         {this.props.businesses.map(business => {
           return <Business business={business} key={business.id} />;
         })}
-      </BusinessListDiv>
+      </div>
     );
   }
 }
