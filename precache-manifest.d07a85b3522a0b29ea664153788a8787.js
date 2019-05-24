c89e9d75ec203fdc6461f6237d1756bebcfa8df3
MFAC <ninafroes@gmail.com>
2019-05-24 13:31:23 +0200

    Updates

--- precache-manifest.d07a85b3522a0b29ea664153788a8787.js
+++ precache-manifest.d07a85b3522a0b29ea664153788a8787.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "6ff506f176a61ca7579d",
-    "url": "/Ravenous-Codecademy/static/css/main.be2fc42b.chunk.css"
+    "revision": "51a65e8503cb24a78294",
+    "url": "/Ravenous-Codecademy/static/css/main.9160477c.chunk.css"
   },
   {
-    "revision": "6ff506f176a61ca7579d",
-    "url": "/Ravenous-Codecademy/static/js/main.6ff506f1.chunk.js"
+    "revision": "51a65e8503cb24a78294",
+    "url": "/Ravenous-Codecademy/static/js/main.51a65e85.chunk.js"
   },
   {
-    "revision": "0cedf2e57300fc359144",
-    "url": "/Ravenous-Codecademy/static/js/1.0cedf2e5.chunk.js"
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/Ravenous-Codecademy/static/js/1.fbfa9ac1.chunk.js"
   },
   {
     "revision": "0a25412bce257970c8fe",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/Ravenous-Codecademy/static/media/background_search_desktop.b1b8fd92.jpg"
   },
   {
-    "revision": "9b02a7cff0c78bf81bcfc4aa3872a112",
+    "revision": "ce181ab2f038d576cc4a41b6f6d6bbd3",
     "url": "/Ravenous-Codecademy/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Ravenous-Codecademy/precache-manifest.b6d0dce58b358e7ee83934638c685369.js"
+  "/Ravenous-Codecademy/precache-manifest.d07a85b3522a0b29ea664153788a8787.js"
 );
 
 workbox.clientsClaim();
